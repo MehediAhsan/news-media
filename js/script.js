dd144d7c08c8c83f5f1321dea163728bd7108d25
Md Mehedi Ahsan <ahsanmehedi041@gmail.com>
2022-09-03 03:11:24 +0600

    news details added

--- js/script.js
+++ js/script.js
@@ -48,7 +48,7 @@ const displayNews = allNews => {
     newsContainer.textContent = '';
     allNews.forEach(news => {
         console.log(news);
-        const {thumbnail_url,title,details,author,total_view} = news;
+        const {thumbnail_url,title,details,author,total_view,_id} = news;
         const {name,img,published_date} = author;
         const newsDiv = document.createElement('div');
         newsDiv.classList.add('card','lg:card-side','bg-base-100','shadow-xl','mb-6');
@@ -60,13 +60,13 @@ const displayNews = allNews => {
           <div class="flex justify-between">
           <div class="flex">
           <img class="w-10 h-10 rounded-full mr-3" src="${img}" alt="Album">
-          <h3>${name}</h3>
+          <h3>${name ? name : 'Not available'}</h3>
           </div>
           <div>
-          <h3>${total_view}</h3>
+          <h3>${total_view ? total_view : 'Not available'}</h3>
           </div>  
           <div class="card-actions justify-end">
-            <button class="btn btn-primary">Details</button>
+            <label onclick="loadNewsDetails('${_id}')" for="my-modal-4" class="btn modal-button btn-primary">Details</label>
           </div>
           </div> 
         </div>        
@@ -76,3 +76,33 @@ const displayNews = allNews => {
 }
 
 loadNews();
+
+
+// display news details
+
+
+const loadNewsDetails = async(news_id) => {
+    const url = `https://openapi.programming-hero.com/api/news/${news_id}`;
+    try{
+        const res = await fetch(url);
+        const data = await res.json();
+        displayNewsDetails(data.data[0]);
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+
+const displayNewsDetails = news => {
+    const {title,details,author,total_view,image_url} = news;
+    const {name,img,published_date} = author;
+    console.log(news);
+    const newsDetails = document.getElementById('news-details');
+    newsDetails.textContent = '';
+    newsDetails.innerHTML = `
+    <h3 class="text-lg font-bold">${title}</h3>
+    <figure><img class="p-6" src="${image_url}" alt="Album"></figure>
+    <p class="text-gray-500">${details.slice(0,500)}</p>    
+    `
+}
